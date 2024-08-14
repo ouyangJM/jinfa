601f6068aae3fddef2d408d172ee78e73341f281
liamouyang <liamouyang@cherrypicks.com>
2024-08-14 15:14:03 +0800

    [project] update code format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,22 @@
-import eslintConfigPrettier from "eslint-config-prettier";
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-    "prettier"
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "@stylistic/js",
+    "plugin:prettier/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh',"prettier"],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.3" } },
+  plugins: ["react-refresh", "prettier"],
   rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    indent: "error",
+    "react/jsx-no-target-blank": "off",
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    "@stylistic/js/indent": ["error", 2],
     "prettier/prettier": "error",
   },
-  eslintConfigPrettier
-}
+};
